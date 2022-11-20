1a1f34edf6ef89810df083c70d1fbb2befc843f2
eungwang <scm323@naver.com>
2022-11-20 20:22:35 +0900

    Feat: create confirm-email page

---
+++ pages/register/confirm.tsx
@@ -0,0 +1,88 @@
+import ErrorText from '@components/common/layout/errorText/ErrorText';
+import { useSendVerificationMailMutation } from '@lib/graphql/user/hook/useUser';
+import palette from '@styles/palette';
+import { Button, Input, message } from 'antd';
+import React, { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import styled from 'styled-components';
+import { SendVerificationMailInput } from 'types';
+
+const Confirm = () => {
+  const { control, handleSubmit, formState } =
+    useForm<SendVerificationMailInput>({ defaultValues: { email: '' } });
+  const [sendVerificationMailMutation] = useSendVerificationMailMutation();
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const onSubmit = async (data: SendVerificationMailInput) => {
+    const { email } = data;
+    const result = await sendVerificationMailMutation({
+      variables: {
+        input: { email },
+      },
+    });
+    if (result.data) {
+      const { sendVerificationMail } = result.data;
+      if (sendVerificationMail.ok === true) {
+        message.success({ content: '인증메일이 전송되었습니다.' });
+        setButtonDisabled(true);
+      }
+    }
+  };
+  return (
+    <ConfirmContainer onSubmit={handleSubmit(onSubmit)}>
+      <h1>가입을 위해 이메일을 인증해주세요.</h1>
+      <div className="email-confirm-input-wrapper">
+        <label className="email-confirm-input-label">이메일</label>
+        <Controller
+          control={control}
+          name="email"
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input onChange={field.onChange} type="email" />
+          )}
+        />
+        {formState.errors['email']?.type === 'required' && (
+          <ErrorText
+            content="이메일을 입력해주세요."
+            className="register-error-text"
+          />
+        )}
+      </div>
+      <Button type="primary" htmlType="submit" disabled={buttonDisabled}>
+        이메일 인증요청
+      </Button>
+    </ConfirmContainer>
+  );
+};
+
+export default Confirm;
+
+const ConfirmContainer = styled.form`
+  h1 {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  margin-top: 50px;
+  .email-confirm-input-wrapper {
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+  .email-confirm-input-label {
+    margin-top: 20px;
+    margin-bottom: 5px;
+    color: ${palette.gray_700};
+  }
+  input {
+    width: 400px;
+  }
+  button {
+    margin-top: 20px;
+  }
+  .register-error-text {
+    margin-top: 5px;
+  }
+`;
