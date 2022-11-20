296763c03ce401978cca80dcf10bddcee5597520
eungwang <scm323@naver.com>
2022-11-20 20:23:50 +0900

    Feat: create error-text component

---
+++ src/components/common/layout/errorText/ErrorText.tsx
@@ -0,0 +1,18 @@
+import palette from '@styles/palette';
+import React from 'react';
+import styled from 'styled-components';
+
+interface ErrorTextProps {
+  content: string;
+  className?: string;
+}
+
+const ErrorText: React.FC<ErrorTextProps> = ({ content, className }) => {
+  return <ErroTextContainer className={className}>{content}</ErroTextContainer>;
+};
+
+export default ErrorText;
+
+const ErroTextContainer = styled.span`
+  color: ${palette.red_500};
+`;
