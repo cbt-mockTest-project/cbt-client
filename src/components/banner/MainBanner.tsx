c24eb2d9505c8f9eb7111e9a5775bf3be57aadd8
eungwang <scm323@naver.com>
2022-11-23 23:42:47 +0900

    Feat: create main-banner component

---
+++ src/components/banner/MainBanner.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
+
+const MainBanner = () => {
+  return (
+    <MainBannerContainer>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination]}
+        className="home-main-banner-swiper"
+      >
+        <SwiperSlide>
+          <iframe
+            src="https://ads-partners.coupang.com/widgets.html?id=620466&template=carousel&trackingCode=AF8104485&subId=&width=1024&height=180"
+            width="1024"
+            height="180"
+            frameBorder="0"
+            scrolling="no"
+            referrerPolicy="unsafe-url"
+          ></iframe>
+        </SwiperSlide>
+        <SwiperSlide>
+          <iframe
+            src="https://ads-partners.coupang.com/widgets.html?id=620466&template=carousel&trackingCode=AF8104485&subId=&width=1024&height=180"
+            width="1024"
+            height="180"
+            frameBorder="0"
+            scrolling="no"
+            referrerPolicy="unsafe-url"
+          ></iframe>
+        </SwiperSlide>
+        <SwiperSlide>
+          <iframe
+            src="https://ads-partners.coupang.com/widgets.html?id=620466&template=carousel&trackingCode=AF8104485&subId=&width=1024&height=180"
+            width="1024"
+            height="180"
+            frameBorder="0"
+            scrolling="no"
+            referrerPolicy="unsafe-url"
+          ></iframe>
+        </SwiperSlide>
+      </Swiper>
+    </MainBannerContainer>
+  );
+};
+
+export default MainBanner;
+
+const MainBannerContainer = styled.div`
+  .home-main-banner-swiper {
+    width: 100%;
+    height: 180px;
+  }
+`;
