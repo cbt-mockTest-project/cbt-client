c3b65f051064138f4d900d5373587ac35d3884e9
eungwang <scm323@naver.com>
2022-11-30 07:17:31 +0900

    Feat: redux setting

--- src/modules/redux/slices/index.ts
+++ src/modules/redux/slices/index.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import coreSlice from './core';
 
-const rootReducer = combineReducers({ counter: coreSlice.reducer });
+const rootReducer = combineReducers({ core: coreSlice.reducer });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
