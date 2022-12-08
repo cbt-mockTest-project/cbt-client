7a3915b2d74b049932694eedde93aa4d02fce34c
eungwang <scm323@naver.com>
2022-12-09 00:08:24 +0900

    Feat: add props to confirm-modal

--- src/components/common/modal/ConfirmModal.tsx
+++ src/components/common/modal/ConfirmModal.tsx
@@ -3,11 +3,14 @@ import React, { ComponentProps } from 'react';
 import styled from 'styled-components';
 import Modal from './Modal';
 
+type ContentType = string | string[] | JSX.Element;
 interface ConfirmModalProps
   extends Pick<ComponentProps<typeof Modal>, 'open' | 'onClose'> {
-  content: string | string[];
+  content: ContentType;
   onConfirm: () => void;
   onCancel: () => void;
+  confirmLabel?: string;
+  cancelLabel?: string;
 }
 
 const ConfirmModal: React.FC<ConfirmModalProps> = ({
@@ -16,20 +19,35 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({
   onCancel,
   onConfirm,
   content,
+  confirmLabel = '확인하기',
+  cancelLabel = '취소하기',
 }) => {
+  const Content: React.FC<{ data: ContentType }> = ({ data }) => {
+    if (Array.isArray(data)) {
+      return (
+        <>
+          {data.map((value, index) => (
+            <p key={index}>{value}</p>
+          ))}
+        </>
+      );
+    }
+    if (typeof data === 'string') {
+      return <p>{data}</p>;
+    }
+    return data;
+  };
   return (
     <ConfirmModalContainer>
       <Modal open={open} onClose={onClose}>
         <div className="confirm-modal-wrapper">
           <div className="confirm-modal-content">
-            {typeof content === 'string'
-              ? content
-              : content.map((value, index) => <p key={index}>{value}</p>)}
+            <Content data={content} />
           </div>
           <div className="confirm-modal-button-wrapper">
-            <Button onClick={onCancel}>취소하기</Button>
+            <Button onClick={onCancel}>{cancelLabel}</Button>
             <Button onClick={onConfirm} type="primary">
-              확인하기
+              {confirmLabel}
             </Button>
           </div>
         </div>
