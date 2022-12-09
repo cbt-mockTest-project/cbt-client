d7b3c228d9901ae0d584f87b6ae3d21e507a96e5
eungwang <scm323@naver.com>
2022-12-10 01:30:15 +0900

    Feat: create exam-achievement-result-list component

--- src/components/exam/result/ExamAchievementResult.tsx
+++ src/components/exam/result/ExamAchievementResult.tsx
@@ -1,41 +1,21 @@
 import BasicBox from '@components/common/box/BasicBox';
-import { useReadQuestionsByExamId } from '@lib/graphql/user/hook/useExamQuestion';
-import { convertStateToIcon } from '@lib/utils/utils';
-import palette from '@styles/palette';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import ExamAchievementResultList from '../common/ExamAchievementResultList';
 
-const ExamAchievementResult = () => {
-  const router = useRouter();
-  const [readQuestions, { data: questionQueryData }] =
-    useReadQuestionsByExamId();
-  useEffect(() => {
-    readQuestions({
-      variables: {
-        input: {
-          id: Number(router.query.e),
-        },
-      },
-    });
-  }, [router.query.e]);
-  if (!questionQueryData) return null;
-  const {
-    readMockExamQuestionsByMockExamId: { questions },
-  } = questionQueryData;
+interface ExamAchievementResultProps {
+  className?: string;
+}
+
+const ExamAchievementResult: React.FC<ExamAchievementResultProps> = ({
+  className,
+}) => {
   return (
     <ExamAchievementResultContainer
-      className="exam-result-achieve-check-box not-draggable"
+      className={`exam-result-achieve-check-box not-draggable ${className}`}
       maxHeight={280}
     >
-      <ul>
-        {questions.map((question, idx) => (
-          <li key={idx}>
-            <p className="achieve-result-index">{idx + 1}. </p>
-            <p>{convertStateToIcon(question.state[0].state)}</p>
-          </li>
-        ))}
-      </ul>
+      <ExamAchievementResultList />
     </ExamAchievementResultContainer>
   );
 };
@@ -44,35 +24,4 @@ export default ExamAchievementResult;
 
 const ExamAchievementResultContainer = styled(BasicBox)`
   margin-top: 20px;
-  ul {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    align-items: center;
-    padding: 10px 0;
-  }
-  li {
-    display: flex;
-    align-items: center;
-    gap: 5px;
-
-    p {
-      width: 25px;
-      text-align: center;
-    }
-    .circle-icon {
-      width: 80% !important;
-      color: ${palette.antd_blue_02};
-    }
-    .triangle-icon {
-      color: ${palette.yellow_500};
-    }
-    .clear-icon {
-      width: 100% !important;
-      color: ${palette.red_500};
-    }
-  }
-  .achieve-result-index {
-    margin-left: 20px;
-  }
 `;
