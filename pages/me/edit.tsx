633eaf016114ce9e19742516fb94fbd20a96d6e8
eungwang <scm323@naver.com>
2022-12-17 02:23:14 +0900

    Feat: create edit-profile page

--- pages/me/edit.tsx
+++ pages/me/edit.tsx
@@ -1,56 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
 import { GetServerSideProps, NextPage } from 'next';
 import Layout from '@components/common/layout/Layout';
-import Label from '@components/common/label/Label';
-import { Button, Input } from 'antd';
-import palette from '@styles/palette';
 import { addApolloState, initializeApollo } from '@modules/apollo';
 import { ME_QUERY } from '@lib/graphql/user/query/userQuery';
 import { MeQuery } from '@lib/graphql/user/query/userQuery.generated';
+import EditComponent from '@components/me/edit/EditComponent';
 
 interface EditPageProps {
   user: MeQuery['me']['user'];
 }
 
 const Edit: NextPage<EditPageProps> = ({ user }) => {
-  return (
-    <Layout>
-      <EditBlock>
-        <h1>회원정보 변경</h1>
-        <div className="edit-block">
-          <Label content={'닉네임'} htmlFor="nickname" />
-          <div className="edit-input-and-button-wrapper">
-            <Input id="nickname" defaultValue={user?.nickname} />
-            <Button>변경하기</Button>
-          </div>
-        </div>
-        <div className="edit-block">
-          <Label content={'비밀번호'} />
-          <Label content={'현재 비밀번호'} className="edit-sub-label" />
-          <div className="edit-input-and-button-wrapper">
-            <Input />
-            <Button>확인하기</Button>
-          </div>
-          <Label content={'변경할 비밀번호'} className="edit-sub-label" />
-          <div className="edit-input-and-button-wrapper">
-            <Input />
-            <Button>변경하기</Button>
-          </div>
-        </div>
-        <div className="edit-block">
-          <Label content={'탈퇴하기'} />
-          <div className="edit-input-and-button-wrapper">
-            <Label
-              content={'회원 탈퇴 시 재가입이 불가능 합니다.'}
-              className="edit-sub-label"
-            />
-          </div>
-          <Button className="edit-withdrawal-button">탈퇴하기</Button>
-        </div>
-      </EditBlock>
-    </Layout>
-  );
+  return <Layout>{user && <EditComponent user={user} />}</Layout>;
 };
 
 export default Edit;
@@ -66,29 +27,3 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
   return addApolloState(apolloClient, { props: { user } });
 };
-
-const EditBlock = styled.div`
-  max-width: 300px;
-  margin: 0 auto;
-  h1 {
-    font-size: 1.2rem;
-    font-size: bold;
-  }
-  .edit-input-and-button-wrapper {
-    display: flex;
-    gap: 10px;
-  }
-  .edit-sub-label {
-    font-size: 0.8rem;
-    color: ${palette.gray_500};
-    margin-top: 5px;
-  }
-  .edit-block {
-    display: flex;
-    flex-direction: column;
-    padding: 10px 0;
-  }
-  .edit-withdrawal-button {
-    margin-top: 10px;
-  }
-`;
