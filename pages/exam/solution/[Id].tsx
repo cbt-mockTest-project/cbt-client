aa98130d22acc6f555149a3d11ca2bcf1ec588f5
eungwang <scm323@naver.com>
2023-01-01 02:46:11 +0900

    Feat: Update layout of solution-page

--- pages/exam/solution/[Id].tsx
+++ pages/exam/solution/[Id].tsx
@@ -11,7 +11,7 @@ import {
   ellipsisText,
 } from '@lib/utils/utils';
 import palette from '@styles/palette';
-import { Button, message } from 'antd';
+import { Button, Image, message } from 'antd';
 import ReportModal from '@components/common/modal/ReportModal';
 import { useCreateQuestionFeedBack } from '@lib/graphql/user/hook/useFeedBack';
 import WithHead from '@components/common/head/WithHead';
@@ -22,6 +22,7 @@ import { useMeQuery } from '@lib/graphql/user/hook/useUser';
 import { useAppDispatch } from '@modules/redux/store/configureStore';
 import { coreActions } from '@modules/redux/slices/core';
 import { loginModal } from '@lib/constants';
+import { responsive } from '@lib/utils/responsive';
 
 interface SolutionProps {
   questionsQuery: ReadMockExamQuestionsByMockExamIdQuery;
@@ -86,31 +87,58 @@ const Solution: NextPage<SolutionProps> = ({ questionsQuery }) => {
             {questionsQuery.readMockExamQuestionsByMockExamId.questions.map(
               (el, index) => (
                 <li key={index}>
-                  <pre className="solution-page-question">
-                    {`Q${el.number}. ${el.question}`}
-                    {el.question_img &&
-                      el.question_img.map((el, index) => (
-                        <a
-                          className="solution-page-image-link"
-                          key={index}
-                          href={el.url}
-                          target="_blank"
-                          rel="noreferrer"
-                        >{`이미지${String(index + 1).padStart(2, '0')}`}</a>
-                      ))}
-                  </pre>
-                  <pre className="solution-page-solution">{el.solution}</pre>
-                  <div>
-                    {el.solution_img &&
-                      el.solution_img.map((el, index) => (
-                        <a
-                          className="solution-page-image-link"
-                          key={index}
-                          href={el.url}
-                          target="_blank"
-                          rel="noreferrer"
-                        >{`이미지${String(index + 1).padStart(2, '0')}`}</a>
-                      ))}
+                  <div className="solution-page-question-wrapper">
+                    <div className="solution-page-question-pre-wrapper">
+                      <pre className="solution-page-question">
+                        {`Q${el.number}. ${el.question}`}
+                        {el.question_img && el.question_img.length >= 1 && (
+                          <a
+                            className="solution-page-image-link"
+                            key={index}
+                            href={el.question_img[0].url}
+                            target="_blank"
+                            rel="noreferrer"
+                          >{`이미지${String(index + 1).padStart(2, '0')}`}</a>
+                        )}
+                      </pre>
+                    </div>
+                    {el.question_img && el.question_img.length >= 1 && (
+                      <div className="solution-page-question-image-wrapper">
+                        <Image
+                          src={el.question_img[0].url}
+                          alt="question_image"
+                          className="solution-page-question-image"
+                        />
+                      </div>
+                    )}
+                  </div>
+                  <div className="solution-page-question-wrapper">
+                    <div className="solution-page-solution-pre-wrapper">
+                      <pre className="solution-page-question">
+                        <b>[solution]</b>
+                        <br />
+                        <br />
+                        {`${el.question}`}
+                        {el.solution_img && el.solution_img.length >= 1 && (
+                          <a
+                            className="solution-page-image-link"
+                            key={index}
+                            href={el.solution_img[0].url}
+                            target="_blank"
+                            rel="noreferrer"
+                          >{`이미지${String(index + 1).padStart(2, '0')}`}</a>
+                        )}
+                      </pre>
+                    </div>
+                    {el.solution_img && el.solution_img.length >= 1 && (
+                      <div className="solution-page-question-image-wrapper">
+                        <Image
+                          src={el.solution_img[0].url}
+                          alt="question_image"
+                          className="solution-page-question-image"
+                        />
+                      </div>
+                    )}
                   </div>
                   <Button
                     type="primary"
@@ -205,22 +233,63 @@ const SolutionBlock = styled.div`
     padding: 0px 20px 20px 20px;
     font-size: 1.5rem;
   }
-  .solution-page-question {
-    white-space: pre-wrap;
-    margin-top: 20px;
-    padding: 20px;
-    border-radius: 5px;
-    background-color: ${palette.gray_100};
-  }
+
   .solution-page-solution {
     white-space: pre-wrap;
     margin: 20px 0 20px 20px;
   }
-  .solution-page-image-link {
-    margin-left: 20px;
-    color: ${palette.antd_blue_01};
-  }
+
   .solution-page-report-button {
     margin-top: 20px;
+  }
+
+  .solution-page-image-link {
+    display: none;
+    margin-top: 20px;
+    color: ${palette.antd_blue_01};
+  }
+  .solution-page-question {
+    white-space: pre-wrap;
+    b {
+      font-size: 1.1.rem;
+      font-weight: bold;
+    }
+  }
+  .solution-page-question-pre-wrapper {
+    background-color: ${palette.gray_100};
+    border-radius: 5px;
+    padding: 20px;
+    flex: 4;
+  }
+  .solution-page-solution-pre-wrapper {
+    border-radius: 5px;
+    border: 1px solid ${palette.gray_300};
+    padding: 20px;
+    flex: 4;
+  }
+  .solution-page-question-image-wrapper {
+    flex: 6;
+    text-align: center;
+    border: 1px solid ${palette.gray_300};
+  }
+  .solution-page-question-image {
+    width: 100%;
+    max-height: 220px;
+  }
+
+  .solution-page-question-wrapper {
+    display: flex;
+    margin-top: 30px;
+    gap: 20px;
+  }
+
+  @media (max-width: ${responsive.medium}) {
+    .solution-page-image-link {
+      display: block;
+    }
+    .question-image-wrapper,
+    .solution-page-question-image-wrapper {
+      display: none;
+    }
   }
 `;
