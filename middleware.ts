f568de474950ee907931f0d0ce134178251257a5
eungwang <scm323@naver.com>
2022-12-31 09:48:30 +0900

    Feat: change selected-result page rendering from server-side to client-side

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
-  if (request.nextUrl.pathname.startsWith('/me')) {
+  if (
+    request.nextUrl.pathname.startsWith('/me') ||
+    request.nextUrl.pathname.startsWith('/exam/selectedresult')
+  ) {
     if (!request.cookies.get('jwt-token')) {
       return NextResponse.redirect(new URL('/', request.url));
     }
--- pages/exam/selectedresult.tsx
+++ pages/exam/selectedresult.tsx
@@ -1,23 +1,26 @@
 import WithHead from '@components/common/head/WithHead';
 import Layout from '@components/common/layout/Layout';
 import SelectedResultComponent from '@components/exam/selectedResult/SelectedResultComponent';
-import { useReadQuestionsByState } from '@lib/graphql/user/hook/useExamQuestion';
-import { READ_QUESTIONS_BY_STATE } from '@lib/graphql/user/query/questionQuery';
-import { ReadMockExamQuestionsByStateQuery } from '@lib/graphql/user/query/questionQuery.generated';
-import { ME_QUERY } from '@lib/graphql/user/query/userQuery';
-import { convertWithErrorHandlingFunc } from '@lib/utils/utils';
-import { addApolloState, initializeApollo } from '@modules/apollo';
-import { GetServerSideProps, NextPage } from 'next';
-import React from 'react';
-import { QuestionState } from 'types';
+import {
+  useLazyReadQuestionsByState,
+  useReadQuestionsByState,
+} from '@lib/graphql/user/hook/useExamQuestion';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
-interface SelectedResultPageProps {
-  questionsQuery: ReadMockExamQuestionsByStateQuery;
-}
+const SelectedResult: NextPage = () => {
+  const router = useRouter();
+  const states = router.query.c ? JSON.parse(String(router.query.c)) : [];
+  const examId = Number(router.query.e);
+  const [readQuestionsQuery, { data: questionsQuery }] =
+    useLazyReadQuestionsByState();
+  useEffect(() => {
+    if (router.isReady) {
+      readQuestionsQuery({ variables: { input: { states, examId } } });
+    }
+  }, [router.isReady]);
 
-const SelectedResult: NextPage<SelectedResultPageProps> = ({
-  questionsQuery,
-}) => {
   return (
     <>
       <WithHead
@@ -34,28 +37,3 @@ const SelectedResult: NextPage<SelectedResultPageProps> = ({
 };
 
 export default SelectedResult;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { query } = context;
-  const apolloClient = initializeApollo({}, String(context.req.headers.cookie));
-  const states: QuestionState[] = JSON.parse(String(query.c));
-  const examId = Number(query.e);
-  const request = async () => {
-    return await apolloClient.query<ReadMockExamQuestionsByStateQuery>({
-      query: READ_QUESTIONS_BY_STATE,
-      variables: {
-        input: {
-          states,
-          examId,
-        },
-      },
-    });
-  };
-  const tryRequest = convertWithErrorHandlingFunc({
-    callback: request,
-  });
-
-  const res = await tryRequest();
-  const questionsQuery = res?.data;
-  return addApolloState(apolloClient, { props: { questionsQuery } });
-};
