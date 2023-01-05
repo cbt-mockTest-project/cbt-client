54a17938203ea6825783dd4e048b1526cff529f6
eungwang <scm323@naver.com>
2023-01-05 20:59:51 +0900

    Feat: create full-comment-fragment

---
+++ src/lib/graphql/user/query/questionCommentFragment.generated.tsx
@@ -0,0 +1,17 @@
+import * as Types from '../../../../types';
+
+import gql from 'graphql-tag';
+export type QusetionCommentPartsFragment = { __typename?: 'MockExamQuestionComment', created_at: any, content: string, likeState: boolean, likesCount: number, id: number, user: { __typename?: 'User', nickname: string } };
+
+export const QusetionCommentPartsFragmentDoc = gql`
+    fragment QusetionCommentParts on MockExamQuestionComment {
+  created_at
+  content
+  likeState
+  likesCount
+  id
+  user {
+    nickname
+  }
+}
+    `;
---
+++ src/lib/graphql/user/query/questionCommentFragment.ts
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const FULL_QUESTION_COMMENT_FRAGMENT = gql`
+  fragment QusetionCommentParts on MockExamQuestionComment {
+    created_at
+    content
+    likeState
+    likesCount
+    id
+    user {
+      nickname
+    }
+  }
+`;
