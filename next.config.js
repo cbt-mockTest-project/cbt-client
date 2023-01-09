1fbdeb8a00f80e96d5fb89909a57287f75dcbba9
eungwang <scm323@naver.com>
2023-01-09 21:54:09 +0900

    Feat: apply pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -19,4 +21,16 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public',
+        },
+      },
+    ],
+  ],
+  nextConfig
+);
