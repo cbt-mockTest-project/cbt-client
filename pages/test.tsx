062782560015167ceb7fe06180538820a51767db
eungwang <scm323@naver.com>
2023-02-26 14:39:31 +0900

    test

---
+++ pages/test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NextPage } from 'next';
+
+const Test: NextPage = () => {
+  return (
+    <div>
+      <a
+        href="https://open.kakao.com/o/gg8L3mad"
+        target="_blank"
+        rel="noreferrer"
+      >
+        asdasd
+      </a>
+    </div>
+  );
+};
+
+export default Test;
