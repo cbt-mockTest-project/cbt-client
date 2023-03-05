66232861a09a85886ab8b254b6e36b1e01a05ffa
eungwang <scm323@naver.com>
2023-03-06 00:30:04 +0900

    Feat: [bookmark-page] 스켈레톤 컴포넌트 제작

---
+++ src/components/me/bookmark/BookmarkedQuestionsComponentSkeleton.tsx
@@ -0,0 +1,52 @@
+import { RoundCheckboxGroupOnChangeValueType } from '@components/common/checkbox/RoundCheckboxGroup';
+import { useReadExamTitleAndIdOfBookmarkedQuestion } from '@lib/graphql/user/hook/useQuestionBookmark';
+import { useLazyReadQuestionsByExamId } from '@lib/graphql/user/hook/useExamQuestion';
+import { checkboxOption } from 'customTypes';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import ExamSolutionList from '@components/exam/solution/ExamSolutionList';
+import { Button, Select } from 'antd';
+import { responsive } from '@lib/utils/responsive';
+import SkeletonBox from '@components/common/skeleton/SkeletonBox';
+import ExamSolutionListSkeleton from '@components/exam/solution/ExamSolutionListSkeleton';
+
+interface BookmarkedQuestionsComponentSkeletonProps {}
+
+const BookmarkedQuestionsComponentSkeleton: React.FC<
+  BookmarkedQuestionsComponentSkeletonProps
+> = () => {
+  return (
+    <BookmarkedQuestionsComponentSkeletonBlock>
+      <SkeletonBox width="300px" height="30px" />
+      <div>
+        <SkeletonBox
+          className="bookmark-question-solution-all-hide-button"
+          width="130px"
+          height="32px"
+        />
+      </div>
+      {[1, 2, 3, 4, 5].map((el) => (
+        <ExamSolutionListSkeleton key={el} />
+      ))}
+    </BookmarkedQuestionsComponentSkeletonBlock>
+  );
+};
+
+export default BookmarkedQuestionsComponentSkeleton;
+const BookmarkedQuestionsComponentSkeletonBlock = styled.div`
+  .bookmark-question-exam-title-select {
+    width: 300px;
+  }
+  .bookmark-question-solution-all-hide-button {
+    margin-top: 20px;
+    margin-bottom: -10px;
+  }
+  margin-bottom: 20px;
+  padding: 0 15px;
+  li {
+    list-style: none;
+  }
+  @media (max-width: ${responsive.medium}) {
+    padding-top: 20px;
+  }
+`;
