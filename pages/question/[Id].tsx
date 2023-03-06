40ac26eeff8ebb93907ddd58221ff89f8a40a0f5
eungwang <scm323@naver.com>
2023-03-06 22:30:29 +0900

    Feat: question page 제작

---
+++ pages/question/[Id].tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import Layout from '@components/common/layout/Layout';
+import { addApolloState, initializeApollo, useApollo } from '@modules/apollo';
+import {
+  ReadAllQuestionsQuery,
+  ReadMockExamQuestionQuery,
+} from '@lib/graphql/user/query/questionQuery.generated';
+import {
+  READ_QUESTION,
+  READ_ALL_QUESTIONS,
+} from '@lib/graphql/user/query/questionQuery';
+import { convertWithErrorHandlingFunc } from '@lib/utils/utils';
+import WithHead from '@components/common/head/WithHead';
+import dynamic from 'next/dynamic';
+import QuestionComponentSkeleton from '@components/question/QuestionComponentSkeleton';
+
+const QuestionComponent = dynamic(
+  () => import('@components/question/QuestionComponent'),
+  { loading: () => <QuestionComponentSkeleton /> }
+);
+interface QuestionProps {
+  questionQuery: ReadMockExamQuestionQuery;
+}
+
+const Question: NextPage<QuestionProps> = ({ questionQuery }) => {
+  const title =
+    questionQuery.readMockExamQuestion.mockExamQusetion.question.slice(0, 50);
+  return (
+    <>
+      <WithHead
+        title={`${title} | 모두CBT`}
+        pageHeadingTitle={`${title} 상세 페이지`}
+      />
+
+      <Layout>
+        <QuestionComponent questionQuery={questionQuery} />
+      </Layout>
+    </>
+  );
+};
+
+export default Question;
+
+export const getStaticPaths: GetStaticPaths = async (context) => {
+  const apolloClient = initializeApollo({}, '');
+  let paths: { params: { Id: string } }[] = [];
+  try {
+    const res = await apolloClient.query<ReadAllQuestionsQuery>({
+      query: READ_ALL_QUESTIONS,
+    });
+    if (res.data.readAllQuestions.questions) {
+      paths = res.data.readAllQuestions.questions.map((el) => ({
+        params: { Id: String(el.id) },
+      }));
+    }
+    return { paths, fallback: 'blocking' };
+  } catch (err) {
+    return {
+      paths,
+      fallback: 'blocking',
+    };
+  }
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  if (!context.params?.Id) {
+    return {
+      notFound: true,
+    };
+  }
+  const apolloClient = initializeApollo({}, '');
+
+  const request = async () => {
+    return await apolloClient.query<ReadMockExamQuestionQuery>({
+      query: READ_QUESTION,
+      variables: {
+        input: {
+          questionId: Number(context.params?.Id),
+        },
+      },
+    });
+  };
+
+  const tryRequest = convertWithErrorHandlingFunc({
+    callback: request,
+  });
+  const res = await tryRequest();
+  const questionQuery = res ? res.data : null;
+  return addApolloState(apolloClient, {
+    props: { questionQuery },
+    revalidate: 86400,
+  });
+};
