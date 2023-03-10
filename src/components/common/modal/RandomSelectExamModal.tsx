e656fcf693ae897141a463bdb289b13e967c244d
eungwang1 <scm323@naver.com>
2023-03-10 10:06:26 +0900

    Feat: 랜덤모의고사 전체 항목 추가 및 기타버그 수정

--- src/components/common/modal/RandomSelectExamModal.tsx
+++ src/components/common/modal/RandomSelectExamModal.tsx
@@ -1,5 +1,7 @@
+import { ClearOutlined } from '@ant-design/icons';
 import { TitlesAndCategories } from '@components/main/MainComponent';
 import { convertExamTurn } from '@lib/utils/utils';
+import palette from '@styles/palette';
 import { Button, Tag } from 'antd';
 import Select, { DefaultOptionType } from 'antd/lib/select';
 import { useRouter } from 'next/router';
@@ -39,7 +41,7 @@ const RandomSelectExamModal: React.FC<RandomSelectExamModalProps> = ({
       }
     }
   }, [open]);
-  const onCategoryChange = async (value: string) => {
+  const onChangeCategory = async (value: string) => {
     setSelectedExams([]);
     setCategory(value);
     const filteredTitles = titlesAndCategories.filter(
@@ -51,15 +53,35 @@ const RandomSelectExamModal: React.FC<RandomSelectExamModalProps> = ({
         label: convertExamTurn(title.title),
       })
     );
+    titles.unshift({ value: 0, label: '전체' });
     setTitles(titles);
   };
 
+  const onChangeExam = (value: number[]) => {
+    if (value.includes(0)) {
+      setSelectedExams([0]);
+      return;
+    }
+    setSelectedExams(value);
+  };
+
+  const onClearExams = () => {
+    setSelectedExams([]);
+  };
+
   const onStartRandomExam = () => {
+    let es: string;
+    const isAllSelected = selectedExams.includes(0);
+    if (isAllSelected) {
+      es = JSON.stringify(titles.map((title) => title.value));
+    } else {
+      es = JSON.stringify(selectedExams);
+    }
     setRouteLoading(true);
     router.push({
       pathname: '/exam',
       query: {
-        es: JSON.stringify(selectedExams),
+        es,
         q: '1',
         r: false,
         t: 'hi',
@@ -75,18 +97,26 @@ const RandomSelectExamModal: React.FC<RandomSelectExamModalProps> = ({
           <Label content={'시험선택'} />
           <Select
             options={categories}
-            onChange={onCategoryChange}
+            onChange={onChangeCategory}
             value={category}
           />
         </div>
         <div className="random-select-exam-selector-wrapper">
-          <Label content={'회차추가'} />
+          <div className="random-select-exam-turn-label-wrapper">
+            <Label content={'회차추가'} />
+            <button
+              className="random-select-exam-turn-clear-button"
+              onClick={onClearExams}
+            >
+              <ClearOutlined />
+            </button>
+          </div>
           <Select
             className="multiple-random-exam-selector"
             mode="multiple"
             options={titles}
             value={selectedExams}
-            onChange={setSelectedExams}
+            onChange={onChangeExam}
           />
         </div>
         <Button
@@ -107,6 +137,20 @@ export default RandomSelectExamModal;
 
 const RandomSelectExamModalContainer = styled(Modal)`
   max-width: 500px;
+  .random-select-exam-turn-label-wrapper {
+    display: flex;
+    gap: 15px;
+    align-items: center;
+  }
+  .random-select-exam-turn-clear-button {
+    position: relative;
+    top: 5px;
+    :hover {
+      svg {
+        color: ${palette.antd_blue_01};
+      }
+    }
+  }
   .random-select-exam-wrapper {
     display: flex;
     flex-direction: column;
@@ -119,5 +163,11 @@ const RandomSelectExamModalContainer = styled(Modal)`
   }
   .random-select-exam-start-button {
     margin-top: 20px;
+  }
+  .multiple-random-exam-selector {
+    .ant-select-selector {
+      max-height: 250px;
+      overflow-y: auto;
+    }
   }
 `;
