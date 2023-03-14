c33de39db6f5654a8d54c0e5f52b767ed342a712
eungwang <scm323@naver.com>
2023-03-14 19:24:25 +0900

    Feat: 광고위치 변경

--- pages/exam/index.tsx
+++ pages/exam/index.tsx
@@ -1,3 +1,4 @@
+import GoogleAd from '@components/common/ad/GoogleAd';
 import WithHead from '@components/common/head/WithHead';
 import Layout from '@components/common/layout/Layout';
 import ExamSkeleton from '@components/exam/ExamSkeleton';
@@ -23,7 +24,14 @@ const Exam: NextPage = () => {
         title={`${title}모두CBT`}
         pageHeadingTitle={`${title} 문제풀이 페이지`}
       />
-      <Layout>{<ExamComponent />}</Layout>
+      <Layout>
+        {
+          <>
+            <ExamComponent />
+            <GoogleAd type="display" />
+          </>
+        }
+      </Layout>
     </>
   );
 };
