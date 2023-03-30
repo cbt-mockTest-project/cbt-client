065245b71b11d2e3d69092796e9ec2504d1a0344
eungwang1 <scm323@naver.com>
2023-03-30 17:07:13 +0900

    Feat: 기출문제 pdf다운로드 기능 추가

--- src/lib/utils/utils.tsx
+++ src/lib/utils/utils.tsx
@@ -165,6 +165,17 @@ export const loadScript = ({
   });
 };
 
+export const blobToDataUrl = (blob: Blob): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+};
+
 export const shuffleArray = (array: any[]) => {
   return array.sort(() => Math.random() - 0.5);
 };
