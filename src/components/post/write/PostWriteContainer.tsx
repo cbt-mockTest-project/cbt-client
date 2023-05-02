549ceef2d3b19b7e2540e02a4044bf9a15b53cd2
eungwang1 <scm323@naver.com>
2023-05-02 12:29:11 +0900

    Fix: 글수정 안되던 이슈 해결

--- src/components/post/write/PostWriteContainer.tsx
+++ src/components/post/write/PostWriteContainer.tsx
@@ -163,6 +163,7 @@ const PostWriteContainer: React.FC<PostWriteContainerProps> = () => {
   };
   const requestEdit = async ({ title, content, id }: CreatePostInput) => {
     try {
+      console.log('aa');
       if (!id) return;
       if (!formValidate) {
         setFormValidate(true);
