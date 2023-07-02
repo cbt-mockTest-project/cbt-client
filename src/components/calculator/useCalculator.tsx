0ace926d63a98aca7956d2bc81b8b55e4c7e1893
eungwang <scm323@naver.com>
2023-07-03 02:22:20 +0900

    Fix: 계산기 삼각함수 관련 이슈 해결

--- src/components/calculator/useCalculator.tsx
+++ src/components/calculator/useCalculator.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from 'react';
 import * as math from 'mathjs';
+import { LocalStorage } from '@lib/utils/localStorage';
+import shortid from 'shortid';
+import { CALCULATOR_HISTORY } from './calculator.constants';
 
+interface CalculatorHistory {
+  expression: string;
+  result: string;
+  key: string;
+}
 interface CalculatorHookProps {
   inputRef: React.MutableRefObject<HTMLInputElement | null>;
   calculatorRef: React.MutableRefObject<HTMLDivElement | null>;
@@ -8,14 +16,55 @@ interface CalculatorHookProps {
 
 const useCalculator = ({ inputRef, calculatorRef }: CalculatorHookProps) => {
   const [input, setInput] = useState('');
+  const [calculatorHistories, setCalculatorHistories] = useState<
+    CalculatorHistory[]
+  >([]);
   const [cursorPosition, setCursorPosition] = useState(0);
+  const storage = new LocalStorage();
+  const buttons = [
+    '1',
+    '2',
+    '3',
+    '+',
+    '4',
+    '5',
+    '6',
+    '-',
+    '7',
+    '8',
+    '9',
+    '*',
+    '0',
+    '.',
+    '=',
+    '/',
+    'C',
+    '(',
+    ')',
+    '^',
+    'sin',
+    'cos',
+    'tan',
+    'e',
+  ];
   const calculate = () => {
     try {
       const inputInDegrees = input
         .replace(/(\d+)\^(\d+)/g, 'pow($1, $2)')
         .replace(/(sin|cos|tan)\(([^)]+)\)/g, '$1($2 deg)');
       const answer = math.evaluate(inputInDegrees);
+      if (answer.toString() === input) return;
       setInput(answer.toString());
+      const history = storage.get(CALCULATOR_HISTORY);
+      const newHistory = [
+        {
+          expression: input,
+          result: answer.toString(),
+          key: shortid.generate(),
+        },
+        ...history,
+      ];
+      storage.set(CALCULATOR_HISTORY, newHistory);
     } catch (e) {
       alert('잘못된 수식입니다.');
     }
@@ -33,10 +82,20 @@ const useCalculator = ({ inputRef, calculatorRef }: CalculatorHookProps) => {
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    if (/^[0-9+\-*/().e^ ]*$/.test(value)) {
+    if (/^[0-9+\-*/().e^sin(cos(tan() ]*$/.test(value)) {
       setInput(value);
       setCursorPosition(e.target.selectionStart as number);
     }
+  };
+
+  const handleDeleteCalculatorHistroy = (key: string) => {
+    const histories: CalculatorHistory[] = storage.get(CALCULATOR_HISTORY);
+    histories.splice(
+      histories.findIndex((history) => history.key === key),
+      1
+    );
+    storage.set(CALCULATOR_HISTORY, histories);
+    setCalculatorHistories(histories);
   };
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -99,13 +158,27 @@ const useCalculator = ({ inputRef, calculatorRef }: CalculatorHookProps) => {
     };
   }, [input, cursorPosition, calculatorRef]);
 
+  useEffect(() => {
+    const savedHistories: CalculatorHistory[] = storage.get(CALCULATOR_HISTORY);
+    if (
+      !savedHistories ||
+      savedHistories.length === 0 ||
+      JSON.stringify(savedHistories) === JSON.stringify(calculatorHistories)
+    )
+      return;
+    setCalculatorHistories(savedHistories);
+  }, [storage.get(CALCULATOR_HISTORY)]);
+
   return {
     input,
+    buttons,
     cursorPosition,
     handleMouseUp,
     handleKeyDown,
     handleInput,
     handleClick,
+    calculatorHistories,
+    handleDeleteCalculatorHistroy,
   };
 };
 
