3ea8de431b3868cb32a53d073cc2b25c9cd95641
eungwang <scm323@naver.com>
2023-09-13 08:43:38 +0900

    test

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NextPage } from 'next';
 import { Image } from 'antd';
 import WithHead from '@components/common/head/WithHead';
 
 const Test: NextPage = () => {
+  useEffect(() => {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length) {
+          for (let i = 0; i < mutation.addedNodes.length; i++) {
+            const addedNode = mutation.addedNodes[i] as HTMLElement;
+            console.log(addedNode);
+            if (
+              addedNode.nodeName === 'IFRAME' &&
+              addedNode.getAttribute('src')?.includes('test')
+              // addedNode.getAttribute('src')?.includes('googleads')
+            ) {
+              // 아이프레임에 relative position 설정
+              addedNode.style.position = 'relative';
+
+              // 오버레이 생성
+              const overlay = document.createElement('div');
+              overlay.style.position = 'absolute';
+              overlay.style.top = '0';
+              overlay.style.left = '0';
+              overlay.style.width = '100%';
+              overlay.style.height = '100%';
+              overlay.style.zIndex = '10';
+              overlay.style.cursor = 'not-allowed';
+              overlay.onclick = (e) => {
+                e.preventDefault();
+                alert('클릭이 차단되었습니다.');
+              };
+
+              addedNode.parentElement?.insertBefore(overlay, addedNode);
+            }
+          }
+        }
+      });
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <>
-      <WithHead
-        title="Im"
-        pageHeadingTitle="aa"
-        image="https://dxw2azlbq2ays.cloudfront.net/1676511721279%C3%A1%C2%84%C2%89%C3%A1%C2%85%C2%B3%C3%A1%C2%84%C2%8F%C3%A1%C2%85%C2%B3%C3%A1%C2%84%C2%85%C3%A1%C2%85%C2%B5%C3%A1%C2%86%C2%AB%C3%A1%C2%84%C2%89%C3%A1%C2%85%C2%A3%C3%A1%C2%86%C2%BA%202023-02-16%20%C3%A1%C2%84%C2%8B%C3%A1%C2%85%C2%A9%C3%A1%C2%84%C2%8C%C3%A1%C2%85%C2%A5%C3%A1%C2%86%C2%AB%2010.41.53.png"
-      />
-      <Image
-        src="https://dxw2azlbq2ays.cloudfront.net/1676511721279%C3%A1%C2%84%C2%89%C3%A1%C2%85%C2%B3%C3%A1%C2%84%C2%8F%C3%A1%C2%85%C2%B3%C3%A1%C2%84%C2%85%C3%A1%C2%85%C2%B5%C3%A1%C2%86%C2%AB%C3%A1%C2%84%C2%89%C3%A1%C2%85%C2%A3%C3%A1%C2%86%C2%BA%202023-02-16%20%C3%A1%C2%84%C2%8B%C3%A1%C2%85%C2%A9%C3%A1%C2%84%C2%8C%C3%A1%C2%85%C2%A5%C3%A1%C2%86%C2%AB%2010.41.53.png"
-        alt="image"
-      />
-    </>
+    <iframe
+      width={1000}
+      height={1000}
+      src="http://192.168.219.102:3000/test2"
+    />
   );
 };
 
---
+++ pages/test2.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NextPage } from 'next';
+import { Image } from 'antd';
+import WithHead from '@components/common/head/WithHead';
+import Link from 'next/link';
+
+const Test: NextPage = () => {
+  return (
+    // <a href="https://naver.com" target="_blank" rel="noreferrer">
+    <div style={{ background: 'skyblue', height: '100vh', width: '100vw' }}>
+      asdadasdasdasdas
+    </div>
+    // </a>
+  );
+};
+
+export default Test;
