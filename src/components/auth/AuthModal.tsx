92b7d4a5b498fa552d0872c1468480fc4167a5a2
eungwang <scm323@naver.com>
2023-12-07 01:24:38 +0900

    Feat: auth-modal tab selector개발

--- src/components/auth/AuthModal.tsx
+++ src/components/auth/AuthModal.tsx
@@ -1,4 +1,4 @@
-import { Modal } from 'antd';
+import { Button, Divider, Modal } from 'antd';
 import { ModalProps } from 'antd/lib';
 import React, { useState } from 'react';
 import styled from 'styled-components';
@@ -10,6 +10,17 @@ const AuthModalBlock = styled(Modal)`
   .ant-modal-content {
     width: 330px;
   }
+  .auth-modal-tab-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    button {
+      font-size: 12px;
+      color: #999;
+      cursor: pointer;
+    }
+  }
 `;
 
 interface AuthModalProps extends Omit<ModalProps, 'children'> {}
@@ -18,12 +29,31 @@ const AuthModal: React.FC<AuthModalProps> = (props) => {
   const [currentTab, setCurrentTab] = useState<
     'login' | 'register' | 'findPassword'
   >('login');
+  const isLoginTab = currentTab === 'login';
   const { ...modalProps } = props;
   return (
     <AuthModalBlock {...modalProps} footer={false}>
       {currentTab === 'login' && <LoginTab />}
       {currentTab === 'register' && <RegisterTab />}
       {currentTab === 'findPassword' && <FindPasswordTab />}
+      <div className="auth-modal-tab-wrapper">
+        {isLoginTab && (
+          <>
+            <Button type="text" onClick={() => setCurrentTab('register')}>
+              회원가입
+            </Button>
+            <Divider type="vertical" style={{ marginTop: '4px' }} />
+            <Button type="text" onClick={() => setCurrentTab('findPassword')}>
+              비밀번호 찾기
+            </Button>
+          </>
+        )}
+        {!isLoginTab && (
+          <Button type="text" onClick={() => setCurrentTab('login')}>
+            로그인
+          </Button>
+        )}
+      </div>
     </AuthModalBlock>
   );
 };
