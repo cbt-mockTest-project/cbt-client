93013911201ec2125b0ce2c3fdfb824fa41809fc
eungwang <scm323@naver.com>
2023-12-12 08:12:31 +0900

    Feat: typing-mode-item 컴포넌트 제작

---
+++ src/components/typingMode/TypingModeItem.tsx
@@ -0,0 +1,72 @@
+import BasicCard from '@components/common/card/BasicCard';
+import StudyAnswerBox from '@components/study/StudyAnswerBox';
+import StudyControlBox from '@components/study/StudyControlBox';
+import StudyQuestionBox from '@components/study/StudyQuestionBox';
+import useQuestions from '@lib/hooks/useQuestions';
+import { Button, Input } from 'antd';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import SwiperCore from 'swiper';
+import { MockExamQuestion } from 'types';
+const TypingModeItemBlock = styled.div``;
+
+interface TypingModeItemProps {
+  question: MockExamQuestion;
+  number: number;
+  swiper: SwiperCore;
+}
+
+const TypingModeItem: React.FC<TypingModeItemProps> = ({
+  question,
+  number,
+  swiper,
+}) => {
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+  const { saveBookmark, saveQuestionState } = useQuestions();
+  return (
+    <TypingModeItemBlock>
+      <BasicCard>
+        <StudyQuestionBox
+          question={question}
+          saveBookmark={saveBookmark}
+          questionNumber={number}
+        />
+      </BasicCard>
+      <Input.TextArea
+        className="typing-mode-textarea"
+        placeholder="답을 확인하기 전에 먼저 답을 작성해 보세요."
+        autoSize={{ minRows: 3, maxRows: 8 }}
+      />
+
+      <StudyControlBox
+        className="study-control-box"
+        question={question}
+        saveQuestionState={saveQuestionState}
+        swiper={swiper}
+      />
+      <Button
+        className="typing-mode-answer-visible-toggle-button"
+        onClick={() => setIsAnswerVisible((prev) => !prev)}
+      >
+        {isAnswerVisible ? '정답 가리기' : '정답 보기'}
+      </Button>
+      <AnimatePresence>
+        {isAnswerVisible && (
+          <motion.div
+            className="typing-mode-answer-box"
+            initial={{ opacity: 0, translateY: -10 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0, translateY: -10 }}
+          >
+            <BasicCard>
+              <StudyAnswerBox question={question} />
+            </BasicCard>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </TypingModeItemBlock>
+  );
+};
+
+export default TypingModeItem;
