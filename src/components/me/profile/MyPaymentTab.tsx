ac682989e6d6cb5b1987b7a9ec262f829c1d7821
eungwang <scm323@naver.com>
2023-12-19 01:06:02 +0900

    Feat: 결제내역 탭 제작

--- src/components/me/profile/MyPaymentTab.tsx
+++ src/components/me/profile/MyPaymentTab.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import { useGetMyPayments } from '@lib/graphql/hook/usePayment';
+import { convertToKST } from '@lib/utils/utils';
+import { Table } from 'antd';
+import React, { ComponentProps, useMemo } from 'react';
 import styled from 'styled-components';
 
 const MyPaymentTabBlock = styled.div``;
@@ -6,11 +9,66 @@ const MyPaymentTabBlock = styled.div``;
 interface MyPaymentTabProps {}
 
 const MyPaymentTab: React.FC<MyPaymentTabProps> = () => {
+  const { data: paymentsQuery, loading: getPaymentsLoading } =
+    useGetMyPayments();
+
+  const paymentData = useMemo(() => {
+    if (!paymentsQuery?.getMyPayments.payments) return [];
+    return paymentsQuery?.getMyPayments.payments.map((payment) => {
+      console.log(payment.receiptUrl);
+
+      return {
+        key: payment.id,
+        productName: payment.productName,
+        price: payment.price,
+        date: convertToKST(payment.created_at),
+        receiptUrl: payment.receiptUrl ? (
+          <a
+            href={payment.receiptUrl.toString()}
+            target="_blank"
+            rel="noreferrer"
+          >
+            출력
+          </a>
+        ) : (
+          ''
+        ),
+      };
+    });
+  }, [paymentsQuery?.getMyPayments.payments]);
   return (
     <MyPaymentTabBlock>
-      {/* 컴포넌트의 내용을 여기에 작성하세요. */}
+      <Table
+        columns={columns}
+        dataSource={paymentData}
+        loading={getPaymentsLoading}
+        pagination={false}
+      />
     </MyPaymentTabBlock>
   );
 };
 
 export default MyPaymentTab;
+
+const columns: ComponentProps<typeof Table>['columns'] = [
+  {
+    title: '결제항목',
+    dataIndex: 'productName',
+    key: 'productName',
+  },
+  {
+    title: '결제금액',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: '결제일',
+    dataIndex: 'date',
+    key: 'date',
+  },
+  {
+    title: '영수증',
+    dataIndex: 'receiptUrl',
+    key: 'receiptUrl',
+  },
+];
