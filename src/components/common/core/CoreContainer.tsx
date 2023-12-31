4a3d2f828925414cc81c18aceee4d54d2bd6cc31
eungwang <scm323@naver.com>
2023-12-31 14:58:28 +0900

    Chore: 불필요한 로직 제거

--- src/components/common/core/CoreContainer.tsx
+++ src/components/common/core/CoreContainer.tsx
@@ -4,11 +4,8 @@ import {
   useAppDispatch,
   useAppSelector,
 } from '@modules/redux/store/configureStore';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { isServer } from '@lib/utils/utils';
-import { AppInfo } from './CoreContainer.type';
-import AppUpdateNotification from './modal/AppUpdateNotification';
 import AuthModal from '@components/auth/AuthModal';
 
 interface CoreContainerProps {}
@@ -16,31 +13,10 @@ interface CoreContainerProps {}
 const CoreContainer: React.FC<CoreContainerProps> = () => {
   const dispatch = useAppDispatch();
   const { modalName } = useAppSelector((state) => state.core);
-  const [appUpdateNotificationState, setAppUpdateNotificationState] =
-    useState(false);
+
   const onCloseModal = () => {
     dispatch(coreActions.closeModal());
   };
-
-  useEffect(() => {
-    if (isServer()) return;
-    if (!window.Kakao.isInitialized() && window.Kakao) {
-      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-    }
-
-    // app 양방향 통신을 위한 메소드
-    window.appInfoChanged = function (appInfo: AppInfo) {
-      //Todo: 앱 업데이트 알림 - 어플 배포후 적용
-      // if (appInfo.version !== '1.1.1') {
-      //   setAppUpdateNotificationState(true);
-      // } else {
-      //   setAppUpdateNotificationState(false);
-      // }
-    };
-    if (window.PackageInfo) {
-      window.PackageInfo.postMessage('getPackageInfo');
-    }
-  }, []);
 
   return (
     <CoreContainerBlock>
@@ -48,7 +24,6 @@ const CoreContainer: React.FC<CoreContainerProps> = () => {
         <AuthModal open={true} onCancel={onCloseModal} />
       )}
       <div id="portal" />
-      {appUpdateNotificationState && <AppUpdateNotification />}
     </CoreContainerBlock>
   );
 };
