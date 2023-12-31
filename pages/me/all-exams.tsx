4d3721e76cb161c30bcf74395611c91119f6cc82
eungwang <scm323@naver.com>
2023-12-31 15:22:04 +0900

    Faet: all-exams 권한 설정

--- pages/me/all-exams.tsx
+++ pages/me/all-exams.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import React from 'react';
 import WithHead from '@components/common/head/WithHead';
 import MyAllExamsComponent from '@components/me/allExams/AllExamsComponent';
+import withAuth from '@lib/hocs/withAuth';
 
 interface MyAllExamsProps {}
 
@@ -17,4 +18,4 @@ const MyAllExams: NextPage<MyAllExamsProps> = () => {
   );
 };
 
-export default MyAllExams;
+export default withAuth(MyAllExams);
