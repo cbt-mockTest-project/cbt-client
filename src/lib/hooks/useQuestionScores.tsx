39e44d58f2cd6717757354ced0dae09d7b22bf1d
eungwang <scm323@naver.com>
2024-01-06 16:23:45 +0900

    Feat: useQuestionScores hook 개발

---
+++ src/lib/hooks/useQuestionScores.tsx
@@ -0,0 +1,53 @@
+import { useReadExamTitleAndIdOfBookmarkedQuestion } from '@lib/graphql/hook/useQuestionBookmark';
+import {
+  useReadExamTitleAndIdByState,
+  useResetAllQuestionState,
+} from '@lib/graphql/hook/useQuestionState';
+import { handleError } from '@lib/utils/utils';
+import { Modal, message } from 'antd';
+import { useMemo } from 'react';
+import useQuestions from './useQuestions';
+
+const useQuestionScores = () => {
+  const { resetQuestions } = useQuestions();
+  const { data: readExamTitlesQuery, loading: getExamTitlesLoading } =
+    useReadExamTitleAndIdByState();
+  const [resetAllQuestionStates] = useResetAllQuestionState();
+
+  const examTitles = useMemo(() => {
+    if (!readExamTitlesQuery?.readExamTitleAndIdByQuestionState.ok) return [];
+    return readExamTitlesQuery.readExamTitleAndIdByQuestionState.titleAndId.map(
+      (el) => ({
+        value: Number(el.id),
+        label: String(el.title),
+      })
+    );
+  }, [readExamTitlesQuery]);
+
+  const resetQuestionScores = async () => {
+    Modal.confirm({
+      title: '정말로 점수를 모두 삭제하시겠습니까?',
+      content: '삭제된 점수는 복구할 수 없습니다.',
+      okText: '삭제',
+      cancelText: '취소',
+      onOk: async () => {
+        try {
+          await resetAllQuestionStates();
+          resetQuestions();
+          message.success('점수가 삭제되었습니다.');
+        } catch (e) {
+          message.error('점수 삭제에 실패했습니다.');
+          handleError(e);
+        }
+      },
+    });
+  };
+
+  return {
+    examTitles,
+    getExamTitlesLoading,
+    resetQuestionScores,
+  };
+};
+
+export default useQuestionScores;
