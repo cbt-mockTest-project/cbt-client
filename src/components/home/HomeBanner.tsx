16322caa0d55f667812269bc2c291e25cf4d41e8
eungwang1 <scm323@naver.com>
2024-01-08 10:59:23 +0900

    Feat: 반응형 배너 대응

--- src/components/home/HomeBanner.tsx
+++ src/components/home/HomeBanner.tsx
@@ -1,3 +1,4 @@
+import { responsive } from '@lib/utils/responsive';
 import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
@@ -5,10 +6,36 @@ import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const HomeBannerBlock = styled.div`
-  .home-banner-image-wrapper {
-    position: relative;
-    width: 100%;
-    aspect-ratio: 1024 / 180;
+  .home-banner-image-swiper {
+    .home-banner-image-wrapper {
+      position: relative;
+      width: 100%;
+      aspect-ratio: 1024 / 180;
+    }
+    .visible-on-pc {
+      display: block;
+    }
+    .visible-on-mobile {
+      display: none;
+    }
+  }
+  .home-banner-image-swiper-mobile {
+    display: none;
+  }
+  @media (max-width: ${responsive.small}) {
+    .home-banner-image-swiper {
+      .home-banner-image-wrapper {
+        position: relative;
+        width: 100%;
+        aspect-ratio: 2000 / 650;
+      }
+      .visible-on-pc {
+        display: none;
+      }
+      .visible-on-mobile {
+        display: block;
+      }
+    }
   }
 `;
 
@@ -17,7 +44,10 @@ interface HomeBannerProps {}
 const HomeBanner: React.FC<HomeBannerProps> = () => {
   const banners = [
     {
-      img: '/png/banner/ehs-banner-pc01.png',
+      img: {
+        pc: '/png/banner/ehs-banner-pc01.png',
+        mobile: '/png/banner/ehs-banner-mobile01.png',
+      },
       link: 'https://ehs-master.com',
     },
   ];
@@ -32,11 +62,18 @@ const HomeBanner: React.FC<HomeBannerProps> = () => {
         speed={1000}
         loop={true}
         modules={[Autoplay]}
+        className="home-banner-image-swiper"
       >
         {banners.map((banner) => (
-          <SwiperSlide key={banner.img}>
-            <div className="home-banner-image-wrapper">
-              <Image src={banner.img} alt="배너" fill />
+          <SwiperSlide
+            key={banner.img.pc}
+            className="home-banner-image-swiper-slide"
+          >
+            <div className="home-banner-image-wrapper visible-on-pc">
+              <Image src={banner.img.pc} alt="배너" fill />
+            </div>
+            <div className="home-banner-image-wrapper visible-on-mobile">
+              <Image src={banner.img.mobile} alt="배너" fill />
             </div>
           </SwiperSlide>
         ))}
