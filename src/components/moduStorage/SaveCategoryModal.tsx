fbd1b5fb2fad9ef35d0928c475a97fda7d2acf4b
eungwang <scm323@naver.com>
2024-01-10 00:51:52 +0900

    feat(storage): isPublic default 수정

--- src/components/moduStorage/SaveCategoryModal.tsx
+++ src/components/moduStorage/SaveCategoryModal.tsx
@@ -35,7 +35,7 @@ const SaveCategoryModal: React.FC<SaveCategoryModalProps> = (props) => {
 
   const { onClose, categoryId, defaultValues, ...modalProps } = props;
   const [isPublic, setIsPublic] = React.useState<boolean>(
-    defaultValues ? defaultValues.isPublic || false : false
+    defaultValues ? defaultValues.isPublic || true : true
   );
   const [name, setName] = React.useState<string>(
     defaultValues ? defaultValues.name : ''
