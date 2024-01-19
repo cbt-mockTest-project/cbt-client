8bac752f261c7e93c5497563c771c0cfca5dbf1d
eungwang <scm323@naver.com>
2024-01-20 08:36:38 +0900

    chore(ehs): safe-exam-ids 추가

--- src/lib/constants/ehsMaster.ts
+++ src/lib/constants/ehsMaster.ts
@@ -1,4 +1,6 @@
-export const EHS_SAFE_EXAM_IDS = [1481, 1482];
+export const EHS_SAFE_EXAM_IDS = [
+  1481, 1482, 1480, 1483, 1484, 653, 654, 656, 657, 649, 652,
+];
 export const EHS_CONSTRUCTION_EXAM_IDS = [];
 export const EHS_DANGEROUS_EXAM_IDS = [];
 export const EHS_AIR_EXAM_IDS = [];
