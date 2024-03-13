eef4f119a92d16c98d70d86f0e81f485d889568d
eungwang <scm323@naver.com>
2024-03-13 21:01:46 +0900

    Add WithHead component to TodayQuizPage

--- pages/quiz/today.tsx
+++ pages/quiz/today.tsx
@@ -1,8 +1,17 @@
+import WithHead from '@components/common/head/WithHead';
 import TodayQuizComponent from '@components/today/quiz/TodayQuizComponent';
 import { NextPage } from 'next';
 
 const TodayQuizPage: NextPage = () => {
-  return <TodayQuizComponent />;
+  return (
+    <>
+      <WithHead
+        title={`오늘의 퀴즈 | 모두CBT`}
+        pageHeadingTitle={`오늘의 퀴즈 페이지`}
+      />
+      <TodayQuizComponent />
+    </>
+  );
 };
 
 export default TodayQuizPage;
