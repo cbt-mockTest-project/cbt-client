9a5be934108d804d9cd68cf6741af887fbae9a06
eungwang <scm323@naver.com>
2024-05-03 01:21:05 +0900

    feat: 상품 검색기능 구현'

--- src/components/store/StoreSearch.tsx
+++ src/components/store/StoreSearch.tsx
@@ -1,12 +1,25 @@
 import { Input } from 'antd';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 interface StoreSearchProps {}
 
 const StoreSearch: React.FC<StoreSearchProps> = () => {
+  const router = useRouter();
   return (
     <div>
-      <Input.Search size="large" placeholder="자료를 검색하세요." />
+      <Input.Search
+        size="large"
+        placeholder="자료를 검색하세요."
+        onSearch={(value) => {
+          router.push({
+            query: {
+              ...router.query,
+              s: value,
+            },
+          });
+        }}
+      />
     </div>
   );
 };
