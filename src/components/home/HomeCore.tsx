66e9ff5539d8688c58f9713c5ff5022333372234
eungwang <scm323@naver.com>
2024-05-27 19:47:50 +0900

    feat: 메인페이지 api 호출 재시도 로직 추가

--- src/components/home/HomeCore.tsx
+++ src/components/home/HomeCore.tsx
@@ -12,48 +12,72 @@ const HomeCore: React.FC<HomeCoreProps> = () => {
   const dispatch = useAppDispatch();
   useEffect(() => {
     const fetchPickedCategories = async () => {
-      fetchCategories({
-        limit: 30,
-        examSource: ExamSource.User,
-        isPick: true,
-      }).then((res) => {
-        const categories = res?.data.getExamCategories.categories || [];
-        dispatch(
-          homeActions.setIsPickedCategories({
-            categories: categories as MockExamCategory[],
-          })
-        );
-      });
+      for (let attempt = 0; attempt < 3; attempt++) {
+        try {
+          const res = await fetchCategories(
+            {
+              limit: 30,
+              examSource: ExamSource.User,
+              isPick: true,
+            },
+            'no-cache'
+          );
+          const categories = res?.data.getExamCategories.categories || [];
+          dispatch(
+            homeActions.setIsPickedCategories({
+              categories: categories as MockExamCategory[],
+            })
+          );
+          break; // 성공 시 반복 중단
+        } catch (err) {
+          if (attempt === 2) handleError(err); // 마지막 시도에서 실패한 경우 에러 처리
+        }
+      }
     };
 
     const fetchUserCategories = async () => {
-      const res = await fetchCategories({
-        examSource: ExamSource.User,
-        limit: 30,
-        isPick: false,
-      });
-      const categories = res?.data.getExamCategories.categories || [];
-      const categoriesSortedByLikes = [...categories].sort(
-        (a, b) => b.categoryEvaluations.length - a.categoryEvaluations.length
-      );
-      const categoriesSortedByCreatedAt = [...categories]
-        .sort(
-          (a, b) =>
-            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-        )
-        .map((category) => ({
-          ...category,
-          isNew: true,
-        }));
-      dispatch(
-        homeActions.setUserStorageCategories({
-          categories: [
-            ...categoriesSortedByCreatedAt.slice(0, 2),
-            ...categoriesSortedByLikes.filter((category) => !category['isNew']),
-          ] as MockExamCategory[],
-        })
-      );
+      for (let attempt = 0; attempt < 3; attempt++) {
+        try {
+          const res = await fetchCategories(
+            {
+              examSource: ExamSource.User,
+              limit: 30,
+              isPick: false,
+            },
+            'no-cache'
+          );
+          const categories = res?.data.getExamCategories.categories || [];
+          const categoriesSortedByLikes = [...categories].sort(
+            (a, b) =>
+              b.categoryEvaluations.length - a.categoryEvaluations.length
+          );
+          const categoriesSortedByCreatedAt = [...categories]
+            .sort(
+              (a, b) =>
+                new Date(b.created_at).getTime() -
+                new Date(a.created_at).getTime()
+            )
+            .map((category) => ({
+              ...category,
+              isNew: true,
+            }));
+          dispatch(
+            homeActions.setUserStorageCategories({
+              categories: [
+                ...categoriesSortedByCreatedAt.slice(0, 2),
+                ...categoriesSortedByLikes.filter(
+                  (category) => !category['isNew']
+                ),
+              ] as MockExamCategory[],
+            })
+          );
+          break; // 성공 시 반복 중단
+        } catch (err) {
+          if (attempt === 2) handleError(err); // 마지막 시도에서 실패한 경우 에러 처리
+        }
+      }
     };
+
     try {
       fetchUserCategories();
       fetchPickedCategories();
