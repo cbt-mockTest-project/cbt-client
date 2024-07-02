691b2d051a59e42de3b6ef283ee278c5592ddcbc
eungwang <scm323@naver.com>
2024-07-02 21:51:00 +0900

    feat: 암기장 보안 최적화

--- src/components/category/CategoryUtilButtonWrapper.tsx
+++ src/components/category/CategoryUtilButtonWrapper.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import CategoryUtilButtonBox from './CategoryUtilButtonBox';
 import { useAppSelector } from '@modules/redux/store/configureStore';
-import { Skeleton } from 'antd';
-import useCheckHasCategoryAccess from './hooks/useCheckHasCategoryAccess';
 
 interface CategoryUtilButtonWrapperProps {}
 
 const CategoryUtilButtonWrapper: React.FC<
   CategoryUtilButtonWrapperProps
 > = () => {
-  const { isCategoryAccess } = useCheckHasCategoryAccess();
   const exams = useAppSelector((state) => state.examCategory.category.mockExam);
   const name = useAppSelector((state) => state.examCategory.category.name);
   const categoryId = useAppSelector((state) => state.examCategory.category.id);
@@ -17,7 +14,6 @@ const CategoryUtilButtonWrapper: React.FC<
     (state) => !state.examCategory.category.isPublic
   );
 
-  if (!isCategoryAccess && isPrivate) return <Skeleton />;
   return (
     <CategoryUtilButtonBox
       exams={exams}
--- src/components/category/hooks/useCheckHasCategoryAccess.tsx
+++ src/components/category/hooks/useCheckHasCategoryAccess.tsx
@@ -7,28 +7,31 @@ import {
   CheckHasCategoryAccessMutation,
   CheckHasCategoryAccessMutationVariables,
 } from '@lib/graphql/query/examCategoryBookmark.generated';
+import { SessionStorage } from '@lib/utils/sessionStorage';
+import { message } from 'antd';
+import { useRouter } from 'next/router';
 
 const useCheckHasCategoryAccess = () => {
+  const router = useRouter();
   const categoryId = useAppSelector((state) => state.examCategory.category.id);
+
   const isPublic = useAppSelector(
     (state) => state.examCategory.category.isPublic
   );
   const [isCategoryAccess, setIsCategoryAccess] = useState(false);
   const { data: meQuery } = useMeQuery();
+  const sessionStorage = new SessionStorage();
   const categoryAuthorId = useAppSelector(
     (state) => state.examCategory.category.user.id
   );
 
   useEffect(() => {
-    if (isPublic) {
-      setIsCategoryAccess(true);
+    if (!categoryId || !meQuery || isPublic) return;
+    if (!meQuery.me.user) {
+      message.error('잘못된 접근입니다.');
+      router.replace('/');
       return;
     }
-    if (meQuery?.me?.user?.id === categoryAuthorId) {
-      setIsCategoryAccess(true);
-      return;
-    }
-    if (!categoryId) return;
     (async () => {
       const res = await apolloClient.mutate<
         CheckHasCategoryAccessMutation,
@@ -42,8 +45,12 @@ const useCheckHasCategoryAccess = () => {
         },
       });
       if (res.data?.checkHasCategoryAccess.ok) {
+        sessionStorage.remove('isCheckingCategoryAccess');
         setIsCategoryAccess(true);
+        return;
       }
+      message.error('잘못된 접근입니다.');
+      router.replace('/');
     })();
   }, [categoryAuthorId, meQuery, categoryId, isPublic]);
   return { isCategoryAccess };
